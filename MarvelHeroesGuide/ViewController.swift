1b68b3b9fba0d1ffaefde7223b8b68f4f4b6942c
Tairus Djons <tairusdjons@tairuss-imac.local>
2018-07-19 21:18:52 +0300

    Initial Commit

---
+++ MarvelHeroesGuide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarvelHeroesGuide
+//
+//  Created by Tairus Djons on 19.07.2018.
+//  Copyright © 2018 Tairus Djons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
