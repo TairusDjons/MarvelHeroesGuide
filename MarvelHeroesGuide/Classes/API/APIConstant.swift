802fed5bd8ab87f6aa77804e5c4677f14383baef
Tairus Djons <tairusdjons@tairuss-imac.local>
2018-07-21 03:04:01 +0300

    Add api constants

---
+++ MarvelHeroesGuide/Classes/API/APIConstant.swift
@@ -0,0 +1,17 @@
+//
+//  APIConstant.swift
+//  MarvelHeroesGuide
+//
+//  Created by Tairus Djons on 19.07.2018.
+//  Copyright © 2018 Tairus Djons. All rights reserved.
+//
+
+import Foundation
+
+public class API {
+    
+    // Base Url
+    static let base = "https://gateway.marvel.com/v1/public/comics"
+    
+    static let characters = "https://gateway.marvel.com/v1/public/comics/characters"
+}
