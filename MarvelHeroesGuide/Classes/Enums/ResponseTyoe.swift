f23b26b76a48256175438c8c559d82aa353a6b5b
Tairus Djons <tairusdjons@tairuss-imac.local>
2018-07-21 03:04:55 +0300

    Add response types

---
+++ MarvelHeroesGuide/Classes/Enums/ResponseTyoe.swift
@@ -0,0 +1,14 @@
+//
+//  ResponseTyoe.swift
+//  MarvelHeroesGuide
+//
+//  Created by Tairus Djons on 19.07.2018.
+//  Copyright © 2018 Tairus Djons. All rights reserved.
+//
+
+import Foundation
+
+enum ResponseType {
+    case Json
+    case Data
+}
