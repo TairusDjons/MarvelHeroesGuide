99d7d65a6353b1f79dc61570f2eb2e198cb28ce6
Tairus Djons <tairusdjons@tairuss-imac.local>
2018-07-21 03:11:03 +0300

    Add color struct

---
+++ MarvelHeroesGuide/Classes/Utilities/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  MarvelHeroesGuide
+//
+//  Created by Tairus Djons on 21.07.2018.
+//  Copyright © 2018 Tairus Djons. All rights reserved.
+//
+
+import Foundation
+import UIKit
+struct Colors {
+    static let marvelRed = UIColor(red: 0.93, green: 0.11, blue: 0.14, alpha:1.0)
+}
