f1f4ce451ef3fdfe249beaa7ba561bc8785a1ef1
Tairus Djons <tairusdjons@tairuss-imac.local>
2018-07-21 03:03:30 +0300

    Change tint and background color of navigation bar globally

--- MarvelHeroesGuide/AppDelegate.swift
+++ MarvelHeroesGuide/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let navigationBarAp = UINavigationBar.appearance()
+        navigationBarAp.tintColor = .white
+        navigationBarAp.backgroundColor = Colors.marvelRed
         return true
     }
 
