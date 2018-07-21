2339f457ebe16343cfaeab7288dccfeebbd9aaa7
Tairus Djons <tairusdjons@tairuss-imac.local>
2018-07-21 03:07:14 +0300

    Add event model

---
+++ MarvelHeroesGuide/Classes/Models/Event.swift
@@ -0,0 +1,16 @@
+//
+//  Event.swift
+//  MarvelHeroesGuide
+//
+//  Created by Tairus Djons on 20.07.2018.
+//  Copyright © 2018 Tairus Djons. All rights reserved.
+//
+
+import Foundation
+struct Event {
+    let id: Int
+    let title: String
+    let description: String
+    let thumbnail: Thumbnail
+    let charachters: [Character]
+}
