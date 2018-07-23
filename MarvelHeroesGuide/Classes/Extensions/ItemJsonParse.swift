32dc869ee214dae686e31783de867dd7e9f7db31
Tairus Djons <tairusdjons@tairuss-imac.local>
2018-07-24 02:28:48 +0300

    Add item son parsing

---
+++ MarvelHeroesGuide/Classes/Extensions/ItemJsonParse.swift
@@ -0,0 +1,17 @@
+//
+//  ItemJsonParse.swift
+//  MarvelHeroesGuide
+//
+//  Created by Tairus Djons on 23.07.2018.
+//  Copyright © 2018 Tairus Djons. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+extension Item {
+    init?(json: JSON){
+        self.name = json["name"].stringValue
+        self.resourceURI = json["resourceURI"].stringValue
+    }
+}
