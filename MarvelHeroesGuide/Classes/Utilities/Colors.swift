e477061e13037cb7f616054f3472d33b81bae040
Tairus Djons <tairusdjons@tairuss-imac.local>
2018-07-24 02:36:47 +0300

    Change color intensity

--- MarvelHeroesGuide/Classes/Utilities/Colors.swift
+++ MarvelHeroesGuide/Classes/Utilities/Colors.swift
@@ -9,6 +9,6 @@
 import Foundation
 import UIKit
 struct Colors {
-    static let marvelRed = UIColor(red: 0.93, green: 0.11, blue: 0.14, alpha:1.0)
+    static let marvelRed = UIColor(red: 0.80, green: 0.11, blue: 0.14, alpha:1.0)
     static let backgroundMarvelRed = UIColor(red: 0.42, green: 0.00, blue: 0.01, alpha: 1.0)
 }
