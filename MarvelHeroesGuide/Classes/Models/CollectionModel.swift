4863e25176d454b4202b774093038ce952cfd443
Tairus Djons <tairusdjons@tairuss-imac.local>
2018-07-24 02:31:07 +0300

    Add CollectionModel

---
+++ MarvelHeroesGuide/Classes/Models/CollectionModel.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionModel.swift
+//  MarvelHeroesGuide
+//
+//  Created by Tairus Djons on 23.07.2018.
+//  Copyright © 2018 Tairus Djons. All rights reserved.
+//
+
+import Foundation
+struct CollectionModel {
+    let available: Int
+    let collectionURI: String
+    let items: [Item]
+}
