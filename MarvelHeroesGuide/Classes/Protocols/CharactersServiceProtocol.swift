e59d2478eaba33131f1d3b73f39314f5ef49835e
Tairus Djons <tairusdjons@tairuss-imac.local>
2018-07-24 02:33:48 +0300

    Add new functions

---
+++ MarvelHeroesGuide/Classes/Protocols/CharactersServiceProtocol.swift
@@ -0,0 +1,31 @@
+//
+//  CharachtersServiceProtocol.swift
+//  MarvelHeroesGuide
+//
+//  Created by Tairus Djons on 20.07.2018.
+//  Copyright © 2018 Tairus Djons. All rights reserved.
+//
+
+import Foundation
+
+protocol CharacterServiceProtocol {
+    func getCharacters(uri: String?,
+                       name: String?,
+                       offset: Int?,
+                       limit: Int?,
+                       OnCompletion: @escaping (Result<[Character],Error>)->())
+    
+    func getTotalCharCount(OnCompletion: @escaping (Result<Int, Error>)->())
+    
+    func getCharactersByEvent(event: Event,
+                              offset: Int?,
+                              limit: Int?,
+                              OnCompletion: @escaping (Result<([Character]), Error>)->())
+    
+    
+    func getConnectedCharactersTo(character: Character,
+                                  offset: Int?,
+                                  limit: Int?,
+                                  OnCompletion: @escaping (Result<[Character],Error>)->())
+}
+
