66cbdf8d5a88d380d3d152fb9c8daa10bbf7af32
Tairus Djons <tairusdjons@tairuss-imac.local>
2018-07-24 02:32:12 +0300

    Add item model

---
+++ MarvelHeroesGuide/Classes/Models/item.swift
@@ -0,0 +1,13 @@
+//
+//  item.swift
+//  MarvelHeroesGuide
+//
+//  Created by Tairus Djons on 23.07.2018.
+//  Copyright © 2018 Tairus Djons. All rights reserved.
+//
+
+import Foundation
+struct Item: ResourceItem {
+    let name: String
+    let resourceURI: String
+}
