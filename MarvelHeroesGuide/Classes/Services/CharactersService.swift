e75eafe0516265d5517c3dfce99f2bf95bf5532a
Tairus Djons <tairusdjons@tairuss-imac.local>
2018-07-24 02:35:02 +0300

    Add realisation for new functions

--- MarvelHeroesGuide/Classes/Services/CharactersService.swift
+++ MarvelHeroesGuide/Classes/Services/CharactersService.swift
@@ -10,11 +10,111 @@ import Foundation
 import Alamofire
 import SwiftyJSON
 
+private enum localError: Error {
+    case cannotCreateChar
+}
+
 class CharacterService: MarvelBaseService, CharacterServiceProtocol {
-    func getCharacters( name: String? = nil,
+    
+    
+    func getCharactersByEvent(event: Event,
+                              offset: Int? = 0,
+                              limit: Int? = 20,
+                              OnCompletion: @escaping (Result<[Character], Error>) -> ()) {
+        let uri = event.characters.collectionURI
+        getCharacters(uri: uri, offset: offset, limit: limit) {
+            result in switch result {
+            case .success(let result):
+                OnCompletion(.success(result))
+            case .error(let error):
+                OnCompletion(.error(error))
+            }
+        }
+    }
+    
+    
+    
+    
+    func getConnectedCharactersTo(character: Character,
+                                  offset: Int? = 0,
+                                  limit: Int? = 20,
+                                  OnCompletion: @escaping (Result<[Character], Error>) -> ()) {
+        let api = character.events.collectionURI
+        
+        makeRequest(url: api, method: .get, encoding:URLEncoding.default) {
+            result in switch result {
+            case .success(let result):
+                let json = JSON(result)
+                var counter = 0
+                var dictionary = Dictionary<Character, Int>()
+                var characters = [Character]()
+                var events = [Event]()
+                
+                for(_, subJson) in json["data"]["results"] {
+                    guard let event = Event(json: subJson)
+                        else {continue}
+                    events.append(event)
+                }
+                for event in events {
+                    self.getCharactersByEvent(event: event,
+                                              offset: offset,
+                                              limit: limit) {
+                        result in switch result {
+                        case .success(let result):
+                            for char in result {
+                                if dictionary[char] == nil {
+                                    dictionary[char] = 1
+                                }
+                            }
+                            counter += 1
+                        case .error(let error):
+                            OnCompletion(.error(error))
+                            return
+                        }
+                        if (counter == events.count) {
+                            characters.append(contentsOf: dictionary.keys)
+                            OnCompletion(.success(characters))
+                            return
+                        }
+                    }
+                }
+
+            case .error(let error):
+                OnCompletion(.error(error))
+            }
+        }
+    }
+    
+    
+    
+    func getTotalCharCount(OnCompletion: @escaping (Result<Int, Error>) -> ()) {
+        var params: [String: Any] = [
+            "limit": 0
+        ]
+        makeRequest(url: API.base + API.characters, method: .get, parameters: params, encoding: URLEncoding.default) {
+            result in switch result {
+            case .success(let value):
+                let json = JSON(value)
+                let total = json["data"]["total"].intValue
+                OnCompletion(.success(total))
+            case .error(let error):
+                OnCompletion(.error(error))
+            }
+        }
+    }
+    
+    
+    
+    func getCharacters( uri: String? = nil,
+                        name: String? = nil,
                         offset: Int? = 0,
                         limit: Int? = 20,
                         OnCompletion: @escaping (Result<[Character],Error>)->()) {
+
+        let url: String
+        if uri != nil {url = uri!}
+        else {url = API.base + API.characters}
+        
         var params: [String: Any] = [
             "limit": limit!,
             "offset": offset!
@@ -24,14 +124,13 @@ class CharacterService: MarvelBaseService, CharacterServiceProtocol {
             params["name"] = name
         }
         
-        
-        makeRequest(url: API.characters, method: .get, parameters: params, encoding: URLEncoding.default, responseType: .Json) {
+                
+        makeRequest(url: url, method: .get, parameters: params, encoding: URLEncoding.default, responseType: .Json) {
             result in
             switch result {
             case .success(let value):
                 let json = JSON(value)
                 var characters = [Character]()
-                let js = json["message"].stringValue
                 for(_, subJson) in json["data"]["results"] {
                     guard let char = Character(json: subJson)
                         else {continue}
