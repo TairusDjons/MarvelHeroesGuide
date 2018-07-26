06db1cd5f2b7afe23a0c7cc88aedfd32c72ba5d6
Tairus Djons <tairusdjons@tairuss-imac.local>
2018-07-26 04:03:22 +0300

    Add filter delegate

---
+++ MarvelHeroesGuide/Classes/Protocols/FilterDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  FilterDelegate.swift
+//  MarvelHeroesGuide
+//
+//  Created by Tairus Djons on 26.07.2018.
+//  Copyright © 2018 Tairus Djons. All rights reserved.
+//
+
+import Foundation
+protocol FilterDelegate{
+    func setName(name: String)
+}
