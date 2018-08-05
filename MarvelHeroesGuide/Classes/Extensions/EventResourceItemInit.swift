c25a8f3eb483e7fe50b2d43567be8f99a4ed33bc
Tairus Djons <tairusdjons@tairuss-imac.local>
2018-08-05 22:13:45 +0300

    Add init for Event struct class

---
+++ MarvelHeroesGuide/Classes/Extensions/EventResourceItemInit.swift
@@ -0,0 +1,28 @@
+//
+//  EventResourceItemInit.swift
+//  MarvelHeroesGuide
+//
+//  Created by Tairus Djons on 30.07.2018.
+//  Copyright © 2018 Tairus Djons. All rights reserved.
+//
+
+import Foundation
+
+extension Event {
+    init(_ name: String,
+         _ id: Int,
+         _ description: String,
+         _ thumbnail: Thumbnail? = nil,
+         _ resourceURI: String? = nil,
+         _ urls: [UrlType]? = nil,
+         _ characters: CollectionModel<Character>? = nil) {
+        self.name = name
+        self.id = id
+        self.description = description
+        self.thumbnail = thumbnail!
+        self.resourceURI = resourceURI!
+        self.urls = urls!
+        self.characters = characters!
+    }
+}
+
